6d7551461b1db73a9ac727a32eb837dfa8f63b55
Bastiaan Welmers <bastiaan@welmers.net>
2013-12-08 23:51:31 +0100

    - Extended form - Added handeling of form

--- public/entity.php
+++ public/entity.php
@@ -18,9 +18,32 @@ $entityId = $_REQUEST['entity_id'];
 
 $entity = $da->find($entityName, $entityId);
 
+?>
+<html>
+	<body>
+<?php
 if ($entity instanceof Entity) {
-	print $entity->getView()->getForm();
+	$form = $entity->getView()->getForm();
+	if (!empty($_POST['submit'])) {
+		try {
+			$form->populate($_POST);
+			$em->flush();
+			?><span class="success">Successfully saved entity</span><?php
+		} catch(Exception $e) {
+			?><span class="error">Error: <?php print $e->getMessage(); ?></span><?php
+		}
+	}
+	?><form action="entity.php" method="post">
+	<input type="hidden" name="entity_name" value="<?php print htmlentities($entityName) ?>" />
+	<input type="hidden" name="entity_id" value="<?php print htmlentities($entityId) ?>" />
+	<?php print $form ?>
+	<input type="submit" name="submit" value="Submit" />
+	</form>
+	<?php
 } else {
 	header('HTTP/1.0 404 Not Found');
 	print 'Entity not found';
 }
+?>
+	</body>
+</html>
