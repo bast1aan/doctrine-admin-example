fe93e8eda377252f3aa3bbc72fedeb6d481d1fff
Bastiaan Welmers <bastiaan@welmers.net>
2013-12-26 04:20:40 +0100

    Adjusted index.php so it works with new DoctrineAdmin list view

--- public/index.php
+++ public/index.php
@@ -1,51 +1,18 @@
 <?php
-//require_once(__DIR__ . '/../lib/src/Bast1aan/DoctrineAdmin/Config.php');
-use Bast1aan\DoctrineAdmin\DoctrineAdmin;
-use Bast1aan\DoctrineAdmin\Example;
+
+use Bast1aan\DoctrineAdmin\Example\DoctrineAdmin\MyDoctrineAdmin;
 
 require '../bootstrap.php';
 
-$da = new DoctrineAdmin(entityManager());
-$da->setConfig(new Example\Config());
+$da = new MyDoctrineAdmin(entityManager());
 
 $entities = $da->getCollection('Bast1aan\DoctrineAdmin\Example\Entities\Bug');
-
-$fieldNames = $entities->getFieldNames();
-$associationNames = $entities->getAssociationNames();
-?> 
-<table><tr>
-	<?php
-foreach(array_merge($fieldNames, $associationNames) as $header) {
-	print "<th>$header</th>";
-}
 ?>
-	</tr>
+<html>
+<head><script type="text/javascript" src="js/doctrine_admin.js"></script></head>
+<body>
 <?php
-foreach($entities as $entity) {
-	
-	$assocationColumns = array();
-	$rowSpan = 1;
-	
-	print "<tr>";
-	foreach($fieldNames as $name) {
-		print "<td>"  . $entity->getColumn($name) . "</td>";
-	}
-	foreach($associationNames as $associationName) {
-		$column = $entity->getColumn($associationName);
-		//print $associationName . ' -' . get_class($column) . ' -';exit;
-		print "<td>";
-		if ($column instanceof Iterator) {
-			print "<ul>";
-			foreach($column as $item) {
-				print "<li>$item</li>";
-			}
-			print "</ul>";
-		} else {
-			print (string) $column;
-		}
-		print "</td>";
-	}
-	print "</tr>";
-}
-?>	
-</table>
+print $entities->getEntityList()->render();
+?>
+</body>
+</html>
